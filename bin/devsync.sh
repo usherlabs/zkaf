e94f34b306a84a6f956a83458433841b1055c093
Ryan Soury <ryan@webdoodle.com.au>
2024-03-21 00:58:22 +1100

    no autosync...

--- bin/devsync.sh
+++ bin/devsync.sh
@@ -5,6 +5,4 @@ shift
 
 echo "Synchronising local repository to remote server at $OPERATION...";
 
-while inotifywait -r -e modify,create,delete ./; do
-  rsync -urltv --delete -e ssh ./ $OPERATION:~/zkOracle/
-done
+rsync -urltv --delete -e ssh ./ $OPERATION:~/zkOracle/
