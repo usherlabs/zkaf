720e9198b8e44edc69f61d48bf2b48a8ae6eff0e
Ryan Soury <ryan@webdoodle.com.au>
2024-03-24 22:32:31 +1100

    advance the repo to use a standard manual sync. To refactor the src/main.rs to simply assert and determine proof generation process

--- bin/devsync.sh
+++ bin/devsync.sh
@@ -5,4 +5,4 @@ shift
 
 echo "Synchronising local repository to remote server at $OPERATION...";
 
-rsync -urltv --delete -e ssh ./ $OPERATION:~/zkOracle/
+rsync -azP -e ssh ./ $OPERATION:~/zkoracle/
---
+++ src/main.rs
@@ -0,0 +1,21 @@
+#![no_main]
+#![feature(const_trait_impl)]
+#![feature(effects)]
+
+use ark_ff::Field;
+use ark_pallas::Fq;
+
+// fn pow(a: Fq, n: u32) -> Fq {
+//     (0..n).fold(Fq::ONE, |acc, _| acc * a)
+// }
+
+#[circuit]
+pub fn main(a: Fq, b: Fq) {
+    assert_eq!(a, b)
+}
+
+// pub fn field_arithmetic_example(a: Fq, b: Fq) -> Fq {
+    // let c = (a + b) * a + b * (a + b) * (a + b);
+    // const CONSTANT: Fq = MontFp!("0x12345678901234567890");
+    // c * c * c / (b - a) + pow(a, 2) + CONSTANT
+// }
