cf3b1f2e175f71276644023e811b0c3e5c53448a
Ryan Soury <ryan@webdoodle.com.au>
2024-04-19 01:00:05 +1000

    convert inputs to strings represented as byte array, but receiving error

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@
 #![feature(effects)]
 
 use getrandom_runtime_seeded::init_getrandom;
-use hex;
-use zkaf::utils::verify_proof;
-use std::str;
+// use hex;
+// use zkaf::utils::verify_proof;
+// use std::str;
 
 #[circuit]
-pub fn main(enc_proof: &str, enc_pub_key: &str) -> bool {
-    // use a real seed here from a secure source (not a hardcoded one)
+pub fn main(_proof: &str, _pub_key: &str) -> bool {
+    // TODO: use a real seed here from a secure source (not a hardcoded one)
     init_getrandom([0u8; 32]);
 
     // compile proof verification route
@@ -17,15 +17,19 @@ pub fn main(enc_proof: &str, enc_pub_key: &str) -> bool {
     // then verify it
     // basically the same logic as the verifier in the mpc tls process
 
-    let proof_bytes = hex::decode(enc_proof).unwrap();
-    let pub_key_bytes = hex::decode(enc_pub_key).unwrap();
-    let proof = str::from_utf8(&proof_bytes).unwrap();
-    let pub_key = str::from_utf8(&pub_key_bytes).unwrap();
+    // let proof = String::from_utf8(_proof).unwrap();
+    // let pub_key = String::from_utf8(*_pub_key).unwrap();
+    // let proof_bytes = hex::decode(enc_proof).unwrap();
+    // let pub_key_bytes = hex::decode(enc_pub_key).unwrap();
+    // let proof = str::from_utf8(&proof_bytes).unwrap();
+    // let pub_key = str::from_utf8(&pub_key_bytes).unwrap();
 
-    let res = verify_proof(&proof.to_string(), &pub_key.to_string());
+    // let res = verify_proof(&proof.to_string(), &pub_key.to_string());
 
-    return match res {
-        Ok(_) => true,
-        Err(_) => false,
-    }
+    // return match res {
+    //     Ok(_) => true,
+    //     Err(_) => false,
+    // }
+
+    return true;
 }
